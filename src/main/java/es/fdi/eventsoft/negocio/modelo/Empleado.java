fecc512a0fd5c842a64633b284f42687e947a5e4
Tomas-PC\Tomas <tomini18@hotmail.com>
2017-04-24 23:17:03 +0200

    Configurado Hibernate para conectarse a una base de datos remota proporcionada por Sergio. Toda la configuración está en la carpeta recources/static/persistence.xml. También he creado una entidad Empleado y un main java para probarlo (todo estó está en source).

---
+++ src/main/java/es/fdi/eventsoft/negocio/modelo/Empleado.java
@@ -0,0 +1,85 @@
+package es.fdi.eventsoft.negocio.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Tomas on 24/04/2017.
+ */
+
+@Entity
+@Table(name = "EMPLEADO")
+public class Empleado implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "COD_EMPLEADO")
+    private Long codigo;
+
+    @Column(name = "APELLIDOS")
+    private String apellidos;
+
+    @Column(name = "NOMBRE")
+    private String nombre;
+
+    @Column(name = "FECHA_NACIMIENTO")
+    private Date fechaNacimiento;
+
+    public Empleado() {
+
+    }
+
+    public Empleado(Long codigo, String apellidos, String nombre, Date fechaNacimiento) {
+        this.codigo = codigo;
+        this.apellidos = apellidos;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "codigo=" + codigo +
+                ", apellidos='" + apellidos + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento +
+                '}';
+    }
+}
